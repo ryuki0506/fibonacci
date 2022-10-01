e374370fadfba0b129cb735be79ca81dc61b40f7
Ryuki Ito <ito.r.ao@m.titech.ac.jp>
2022-10-01 20:23:36 +0900

    5th

---
+++ fibonacci.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+int fibonacci(int n){
+    if (n==0)
+        return 0;
+    if (n<=2)
+        return 1;
+    else
+        return 2;
+}
--- test_fibonacci.cpp
+++ test_fibonacci.cpp
@@ -1,5 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
+#include "fibonacci.hpp"
+using namespace std;
 
 TEST_CASE("fibonacci","test"){
-    REQUIRE(2*2==4);
+    vector<tuple<int,int>>cases {{0,0},{1,1},{2,1},{3,2}};
+    for(auto c:cases){
+        REQUIRE(fibonacci(get<0>(c)) == get<1>(c));
+    }
 }
