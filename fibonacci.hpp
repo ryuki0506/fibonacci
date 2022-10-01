1f8eba78edb8402275c4e4016f9e660e02ae851a
Ryuki Ito <ito.r.ao@m.titech.ac.jp>
2022-10-01 20:31:07 +0900

    6th

--- fibonacci.hpp
+++ fibonacci.hpp
@@ -3,8 +3,8 @@
 int fibonacci(int n){
     if (n==0)
         return 0;
-    if (n<=2)
+    if (n==1)
         return 1;
     else
-        return 2;
+        return fibonacci(n-1)+fibonacci(n-2);
 }
--- test_fibonacci.cpp
+++ test_fibonacci.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 TEST_CASE("fibonacci","test"){
-    vector<tuple<int,int>>cases {{0,0},{1,1},{2,1},{3,2}};
+    vector<tuple<int,int>>cases {{0,0},{1,1},{2,1},{3,2},{10,55}};
     for(auto c:cases){
         REQUIRE(fibonacci(get<0>(c)) == get<1>(c));
     }
